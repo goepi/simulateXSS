a757e0bfe98d728e49f94fe6af25210c63f6fe44
goepi <goepi@gmail.com>
2019-11-24 17:39:26 +0100

    Fix content-type

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const getTemplate = require("./template.js");
 
 const server = http.createServer(function(req, res) {
   const data = {
-    username: "</script><script>alert('d')</script>"
+    username: "</script><script>alert('hello')</script>"
   };
   const payloadString = JSON.stringify(data);
   console.log("Sending");
   const template = getTemplate(data);
   console.log(template);
-  res.setHeader("Content-Type", "application/html");
+  res.setHeader("Content-Type", "text/html");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.writeHead(200);
   res.end(template);
